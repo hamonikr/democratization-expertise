d8ca2aebe9c2326aa552997c72b2fb10934ede7f
이영은 <bono6315@invesume.com>
2020-07-16 16:49:26 +0900

    clean
    Former-commit-id: 4d9ba935eaad558e0f22cda862cd9d883c4c3bf2

---
+++ de-community/src/main/java/com/de/question/Questions.java
@@ -0,0 +1,52 @@
+package com.de.question;
+
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "tb_questions")
+public class Questions {
+	
+	/* 질문 고유 번호 */
+	@Id
+	@Column
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private Integer questionNo;
+	/* 질문 수정 권한 */
+	private Integer editAuth;
+	/* 질문 구분 */
+	private String section;
+	/* 질문 이름 */
+	private String title;
+	/* 질문 내용 */
+	private String contents;
+	/* 회원 고유 번호 */
+	private String userNo;
+	/* 태그 고유 번호 */
+	private Integer[] tagNo;
+	/* 질문 조회수 */
+	private Integer readCnt;
+	/* 등록 일시 */
+	@CreationTimestamp
+	private Timestamp registerDate;
+	/* 수정 일시 */
+	@UpdateTimestamp
+	private Timestamp updateDate;
+
+}
