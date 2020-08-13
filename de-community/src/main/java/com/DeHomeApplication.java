d60faecf24d201a7fc7fc043584cb4ac57d743f9
이영은 <bono6315@invesume.com>
2020-08-13 15:25:01 +0900

    change main code
    Former-commit-id: c500fd0f47fb09145fd881390d170e83977b659c [formerly c500fd0f47fb09145fd881390d170e83977b659c [formerly 977491a9c2e2f76a8a0366aea87b3a0e546d58be]]
    Former-commit-id: 6c517151ebe5c445bff89b6f8473b55b6a8da536
    Former-commit-id: 4d03c596a4995af1f2a86ee59238ea7e90ec2b64

--- de-community/src/main/java/com/DeHomeApplication.java
+++ de-community/src/main/java/com/DeHomeApplication.java
@@ -9,48 +9,52 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DeHomeApplication {
-	public static boolean running = true;
-
-	
-	public static void main(String[] args) throws Exception {
+	public static void main(String[] args) {
 		SpringApplication.run(DeHomeApplication.class, args);
-		System.out.println("+++ JavaStopTest started +++");
-		
-		String command = "exit";
-		
-	    Runtime.getRuntime().addShutdownHook(new MyShutdownHook());
-
-		int i = 0;
-	
-		while (running) {
-			System.out.println("count=" + i);
-			i++;
-
-		try {
-			Thread.sleep(1000);
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-	
-			if (i == 50) {
-			// emergency exit
-				running = false;
-			}
-		}
-			
-			System.out.println("--- JavaStopTest ended ---");
-			new ProcessBuilder("/bin/bash", "-c", command).start();
-		}
-
-	 public static void shellCmd(String command) throws Exception {
-         Runtime runtime = Runtime.getRuntime();
-         Process process = runtime.exec(command);
-         InputStream is = process.getInputStream();
-         InputStreamReader isr = new InputStreamReader(is);
-         BufferedReader br = new BufferedReader(isr);
-         String line;
-         while((line = br.readLine()) != null) {
-                        System.out.println(line);
-         }
-	 }
+		System.out.println(">>>> run DeHomeApplication >>>>");
+	}
+//	
+//	public static boolean running = true;
+//	
+//	public static void main(String[] args) throws Exception {
+//		SpringApplication.run(DeHomeApplication.class, args);
+//		System.out.println("+++ JavaStopTest started +++");
+//		
+//		String command = "exit";
+//		
+//	    Runtime.getRuntime().addShutdownHook(new MyShutdownHook());
+//
+//		int i = 0;
+//	
+//		while (running) {
+//			System.out.println("count=" + i);
+//			i++;
+//
+//		try {
+//			Thread.sleep(1000);
+//		} catch (InterruptedException e) {
+//			e.printStackTrace();
+//		}
+//	
+//			if (i == 50) {
+//			// emergency exit
+//				running = false;
+//			}
+//		}
+//			
+//			System.out.println("--- JavaStopTest ended ---");
+//			new ProcessBuilder("/bin/bash", "-c", command).start();
+//		}
+//
+//	 public static void shellCmd(String command) throws Exception {
+//         Runtime runtime = Runtime.getRuntime();
+//         Process process = runtime.exec(command);
+//         InputStream is = process.getInputStream();
+//         InputStreamReader isr = new InputStreamReader(is);
+//         BufferedReader br = new BufferedReader(isr);
+//         String line;
+//         while((line = br.readLine()) != null) {
+//                        System.out.println(line);
+//         }
+//	 }
 }
