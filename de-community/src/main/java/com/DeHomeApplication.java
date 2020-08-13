580fe94ea6fc32473276ab4f0ece3f8628b7b2c7
이영은 <bono6315@invesume.com>
2020-08-13 15:01:54 +0900

    change main
    Former-commit-id: 0f1ba04083d1ab23fb8939b2057978ae79758f5f [formerly 0f1ba04083d1ab23fb8939b2057978ae79758f5f [formerly 068d98035c077f535dc89882db3578c183536cdc]]
    Former-commit-id: 56384f8e774eb2894ddb95816937a334d18e92f3
    Former-commit-id: a76ddba7f6269954c1bf95fb9159686435173fb5

--- de-community/src/main/java/com/DeHomeApplication.java
+++ de-community/src/main/java/com/DeHomeApplication.java
@@ -1,5 +1,9 @@
 package com;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,10 +12,12 @@ public class DeHomeApplication {
 	public static boolean running = true;
 
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(DeHomeApplication.class, args);
 		System.out.println("+++ JavaStopTest started +++");
-		Runtime.getRuntime().addShutdownHook(new MyShutdownHook());
+	    String command = "exit 0";  
+
+	    Runtime.getRuntime().addShutdownHook(new MyShutdownHook());
 
 		int i = 0;
 	
@@ -25,14 +31,25 @@ public class DeHomeApplication {
 			e.printStackTrace();
 		}
 	
-			if (i == 100) {
+			if (i == 50) {
 			// emergency exit
 				running = false;
 			}
 		}
-
+			
 			System.out.println("--- JavaStopTest ended ---");
+			shellCmd(command);	
 		}
 
-	
+	 public static void shellCmd(String command) throws Exception {
+         Runtime runtime = Runtime.getRuntime();
+         Process process = runtime.exec(command);
+         InputStream is = process.getInputStream();
+         InputStreamReader isr = new InputStreamReader(is);
+         BufferedReader br = new BufferedReader(isr);
+         String line;
+         while((line = br.readLine()) != null) {
+                        System.out.println(line);
+         }
+	 }
 }
