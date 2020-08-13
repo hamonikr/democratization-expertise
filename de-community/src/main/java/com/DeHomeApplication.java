c694d5b8a5d3b395e6c4a5dbfcf768d8dbf1798e
이영은 <bono6315@invesume.com>
2020-08-13 14:08:01 +0900

    test time check add

--- de-community/src/main/java/com/DeHomeApplication.java
+++ de-community/src/main/java/com/DeHomeApplication.java
@@ -5,9 +5,34 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DeHomeApplication {
+	public static boolean running = true;
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DeHomeApplication.class, args);
-	}
+		System.out.println("+++ JavaStopTest started +++");
+		Runtime.getRuntime().addShutdownHook(new MyShutdownHook());
 
+		int i = 0;
+	
+		while (running) {
+			System.out.println("count=" + i);
+			i++;
+
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	
+			if (i == 100) {
+			// emergency exit
+				running = false;
+			}
+		}
+
+			System.out.println("--- JavaStopTest ended ---");
+		}
+
+	
 }
---
+++ de-community/src/main/java/com/MyShutdownHook.java
@@ -0,0 +1,18 @@
+package com;
+public class MyShutdownHook extends Thread {
+
+@Override
+public void run() {
+System.out.println("=== my shutdown hook activated");
+
+// stop running threads
+// store data to DB
+// close connection to DB
+// disconnect...
+// release other resources...
+
+DeHomeApplication.running = false;
+
+}
+
+}
