0c976c65b0f5539d4ea3d60aed25dd5790ef4a8c
swkim <swkim@invesume.com>
2020-08-17 22:37:31 +0900

    mybatis 디비 커넥션 수정

--- de-community/src/main/java/com/de/sample/db/DatabaseConfig.java
+++ de-community/src/main/java/com/de/sample/db/DatabaseConfig.java
@@ -3,10 +3,12 @@ package com.de.sample.db;
 import javax.sql.DataSource;
 
 import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -17,7 +19,7 @@ import com.de.sample.GlobalPropertySource;
 
 
 @Configuration
-@MapperScan(basePackages="com.de.sample")
+@MapperScan(basePackages="com.de.*.mapper")
 @EnableTransactionManagement
 public class DatabaseConfig {
  
@@ -35,7 +37,15 @@ public class DatabaseConfig {
             .password(globalPropertySource.getPassword())
             .build();
     }
-   
+    
+    @Bean
+	public SqlSessionFactory db1SqlSessionFactory(ApplicationContext applicationContext) throws Exception {
+		SqlSessionFactoryBean sqlSessionFactoryBean = new SqlSessionFactoryBean();
+		sqlSessionFactoryBean.setDataSource(customDataSource());
+		sqlSessionFactoryBean.setMapperLocations(applicationContext.getResources("classpath:publicMapper/*.xml"));
+
+		return sqlSessionFactoryBean.getObject();
+	}
     
     @Bean
     public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) throws Exception {
