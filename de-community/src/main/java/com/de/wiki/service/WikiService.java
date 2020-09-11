9985129a59ab3fd065446912e9240d6973aeee5e
이영은 <bono6315@invesume.com>
2020-09-11 17:49:34 +0900

    wiki- help create 기능 생성

--- de-community/src/main/java/com/de/wiki/service/WikiService.java
+++ de-community/src/main/java/com/de/wiki/service/WikiService.java
@@ -1,10 +1,30 @@
 package com.de.wiki.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import com.de.wiki.Wiki;
+import com.de.wiki.mapper.WikiMapper;
 
 @Service
 @Transactional
 public class WikiService {
+	@Autowired
+	WikiMapper mapper;
 
+	// 위키문서 저장
+	public Wiki getWikiHelp(Wiki vo)throws Exception{
+		return mapper.getWikiHelp(vo);		
+	};
+
+	
+	// 위키문서 저장
+	public int save(Wiki vo)throws Exception{
+		return mapper.save(vo);		
+	};
+	
+	public int saveHistory(Wiki vo)throws Exception{
+		return mapper.saveHistory(vo);		
+	};
 }
