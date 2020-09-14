649b58436f466db133c25aff5041558c0adfc2f0
goro <goro@bms.com>
2020-09-14 15:38:19 +0900

    로그인 실패시 메세지 출력 추가

--- de-community/src/main/java/com/DeHomeApplication.java
+++ de-community/src/main/java/com/DeHomeApplication.java
@@ -1,7 +1,12 @@
 package com;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 
 @SpringBootApplication
 public class DeHomeApplication {
@@ -9,5 +14,16 @@ public class DeHomeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DeHomeApplication.class, args);
 	}
+	
+	@Bean 
+	public MessageSource messageSource(){ 
+		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource(); 
+		messageSource.setBasename("classpath:/message/message"); 
+		messageSource.setDefaultEncoding("UTF-8"); 
+		messageSource.setCacheSeconds(180);
+		Locale.setDefault(Locale.getDefault()); 
+		
+		return messageSource;
+		}
 
 }
