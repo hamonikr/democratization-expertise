2360f8620286da8901f8bb529271a5ce770077d6
limitationone <hji@invesume.com>
2020-09-22 02:14:57 +0900

    Update 회원 내 답변 더보기 추가

--- de-community/src/main/java/com/de/answer/Answers.java
+++ de-community/src/main/java/com/de/answer/Answers.java
@@ -4,8 +4,6 @@ import java.sql.Timestamp;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
