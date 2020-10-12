d97ae9ab1ecb07ff1d5504091f5def5a622985c2
이영은 <bono6315@invesume.com>
2020-10-12 15:50:07 +0900

    user xml 수정
    Former-commit-id: 2dbeeb0a0fabd6e258abe80fde5e5f5dd9de1354 [formerly 2dbeeb0a0fabd6e258abe80fde5e5f5dd9de1354 [formerly cb19fbf4003a013add0220dc00f0f94e2f2aba65]]
    Former-commit-id: d67cf2319fcf5dcbd2a84bfc36cc496eaab31ac6
    Former-commit-id: af58b37cc7078949a5eb9e2d0c1b4bc48e34dc97

--- de-community/src/main/java/com/de/user/UsersRepository.java
+++ de-community/src/main/java/com/de/user/UsersRepository.java
@@ -1,5 +1,7 @@
 package com.de.user;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -8,4 +10,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface UsersRepository extends JpaRepository<Users, Integer>{
 	Page<Users> findAll(Pageable pageable);
+	Optional<Users> findByUserno(int seq);
+
 }
