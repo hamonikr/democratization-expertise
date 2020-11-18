2b2af56aa54b91aab844228dd0e7f35ea3b03818
이영은 <bono6315@invesume.com>
2020-11-18 19:14:30 +0900

    wiki 수정

--- de-community/src/main/java/com/de/wiki/WikiController.java
+++ de-community/src/main/java/com/de/wiki/WikiController.java
@@ -19,7 +19,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.de.cmmn.CmmnMap;
